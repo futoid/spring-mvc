66605df9462a416473e3fb304d0ca7bff742a07b
futoid <aliekmandal24@gmail.com>
2025-07-23 00:01:56 +0530

    update: post content type

--- mobile-app-ws/src/main/java/com/futoid/spring/mobile/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/futoid/spring/mobile/controller/UserController.java
@@ -1,16 +1,21 @@
 package com.futoid.spring.mobile.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.futoid.spring.MobileAppWsApplication;
+import com.futoid.spring.mobile.model.response.UserDetailsRequestModel;
 import com.futoid.spring.mobile.model.response.UserRef;
 
 @RestController
@@ -29,19 +34,23 @@ public class UserController {
 		return "get user called with page no " + page + " limit " + limit + " sort " + sort;  
 	}
 	
-	@GetMapping(path="/{userId}", produces = {MediaType.APPLICATION_ATOM_XML_VALUE , MediaType.APPLICATION_JSON_VALUE})
-	public UserRef getUser(@PathVariable String userId) {
+	@GetMapping(path="/{userId}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<UserRef> getUser(@PathVariable String userId) {
 		UserRef user = new UserRef();
 		user.setFirstName("alexa");
 		user.setLastName("tois");
 		user.setEmail("alex@alex.com");
-		
-		return user;
+		user.setUserId(userId);		
+		return new ResponseEntity<UserRef>(user, HttpStatus.OK);
 	}
 	
-	@PostMapping
-	public String postUser() {
-		return "post user called";
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<UserRef> postUser(@RequestBody UserDetailsRequestModel userDetails) {
+		UserRef user = new UserRef();
+		user.setFirstName(userDetails.getFirstName());
+		user.setLastName(userDetails.getLastName());
+		user.setEmail(userDetails.getEmail());
+		return new ResponseEntity<UserRef>(user, HttpStatus.OK);
 	}
 	
 	@PutMapping
