526e5ae2fa7b3f68aca081a3c3a17c522067191b
futoid <aliekmandal24@gmail.com>
2025-07-22 23:18:31 +0530

    new: intital spring

---
+++ mobile-app-ws/src/main/java/com/futoid/spring/mobile/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.futoid.spring.mobile.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.futoid.spring.mobile.model.response.UserRef;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	//if required is false a request param is string it will pass null value, for int throws error
+	@GetMapping
+	public String getUser(@RequestParam(defaultValue="3") int page, @RequestParam(defaultValue="50", required = false) int limit, @RequestParam(required=false) String sort) {
+		return "get user called with page no " + page + " limit " + limit + " sort " + sort;  
+	}
+	
+	@GetMapping(path="/{userId}")
+	public UserRef getUser(@PathVariable String userId) {
+		UserRef user = new UserRef();
+		user.setFirstName("alexa");
+		user.setLastName("tois");
+		user.setEmail("alex@alex.com");
+		
+		return user;
+	}
+	
+	@PostMapping
+	public String postUser() {
+		return "post user called";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "udpate user called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user called";
+	}
+	
+}
