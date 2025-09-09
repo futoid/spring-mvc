05a6d0e0714e4cd0d4dcfc40bc5c065281fdaba8
futoid <aliekmandal24@gmail.com>
2025-09-09 22:49:43 +0530

    new: auto map of api gateway

---
+++ ApiDiscoveryService/src/main/java/com/futoid/spring/apidiscovery/ApiDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.futoid.spring.apidiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDiscoveryServiceApplication.class, args);
+	}
+
+}
